143f94306c0bc33fef5aeff6cc42a4c8a30dea4b
Marcin Grzmil <marcin.grzmil@hotmail.com>
2023-07-15 16:01:43 +0200

    added flag icon to readme

--- src/modules/Readme/Readme.tsx
+++ src/modules/Readme/Readme.tsx
@@ -1,5 +1,7 @@
 import { FC } from "react";
 import { useTranslation } from "react-i18next";
+import enFlag from "../../assets/en.svg";
+import plFlag from "../../assets/pl.svg";
 import styles from "./readme.module.css";
 
 const Readme: FC = () => {
@@ -7,7 +9,15 @@ const Readme: FC = () => {
 
   return (
     <div className={styles["wrapper"]}>
-      <h1>{t("headers.readme")}</h1>
+      <span className={styles.header}>
+        <h1>{t("headers.readme")}</h1>
+        {t("headers.readme") == "PL" ? (
+          <img src={plFlag} alt="" />
+        ) : (
+          <img src={enFlag} alt="" />
+        )}{" "}
+        <img src="" alt="" />
+      </span>
       <h1>react-router & custom hooks:</h1>
       <p>
         {t("readme.router1")}
