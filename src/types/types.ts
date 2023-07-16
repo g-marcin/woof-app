3ea7127f0653b6b83e6a3398cca94c1beb6482d6
Marcin Grzmil <marcin.grzmil@hotmail.com>
2023-07-16 13:38:27 +0200

    isolated LanguageToggle and ThemeToggle components

--- src/types/types.ts
+++ src/types/types.ts
@@ -2,15 +2,19 @@ export interface Response {
     status?: 'success' | 'error';
     code?: number;
 }
+
 export interface DogDetailsDTO extends Response {
     message: string;
 }
+
 export interface DogDetails extends Response {
     imageSrc: string;
 }
+
 export interface DogListDTO extends Response {
     message: { [k: string]: [] | string[] };
 }
+
 export type DogEntries = [string, [] | string[]][];
 
 export interface DogVariantsDTO extends Response {
