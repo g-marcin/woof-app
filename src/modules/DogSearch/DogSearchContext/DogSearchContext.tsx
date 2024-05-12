1124a65ecf17ffb06e9037ea1b9b6b40a6eda4a4
Marcin Grzmil <marcin.grzmil@wakacje.pl>
2024-05-12 17:33:16 +0200

    issue01 fix context typing issue

--- src/modules/DogSearch/DogSearchContext/DogSearchContext.tsx
+++ src/modules/DogSearch/DogSearchContext/DogSearchContext.tsx
@@ -3,10 +3,11 @@ import { createContext } from 'react'
 
 type DogSearchContextType = [string, Dispatch<SetStateAction<string>>]
 
-export const DogSearchContext = createContext<DogSearchContextType|undefined>(undefined)
+export const DogSearchContext = createContext<DogSearchContextType>((['', () => {}] as unknown) as DogSearchContextType)
 
 export const DogSearchContextProvider: FC<PropsWithChildren> = ({ children }) => {
     const [searchQuery, setSearchQuery] = useState('')
+
     return (
         <DogSearchContext.Provider value={[searchQuery, setSearchQuery]}>
             {children}
