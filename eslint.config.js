8c25874767c18b64cd9eb4dd3a0f4c8117c36407
mgrzmil <mgrzmil4@gmail.com>
2025-12-20 10:28:40 +0100

    bump deprecated packages

---
+++ eslint.config.js
@@ -0,0 +1,40 @@
+import js from '@eslint/js';
+import tsPlugin from '@typescript-eslint/eslint-plugin';
+import tsParser from '@typescript-eslint/parser';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import globals from 'globals';
+
+export default [
+    {
+        ignores: ['dist', 'node_modules'],
+    },
+    {
+        files: ['**/*.{ts,tsx}'],
+        languageOptions: {
+            ecmaVersion: 'latest',
+            globals: globals.browser,
+            parser: tsParser,
+            parserOptions: {
+                ecmaVersion: 'latest',
+                sourceType: 'module',
+            },
+        },
+        plugins: {
+            '@typescript-eslint': tsPlugin,
+            'react-hooks': reactHooks,
+            'react-refresh': reactRefresh,
+        },
+        rules: {
+            ...js.configs.recommended.rules,
+            ...tsPlugin.configs.recommended.rules,
+            ...reactHooks.configs.recommended.rules,
+            'react-refresh/only-export-components': [
+                'warn',
+                { allowConstantExport: true },
+            ],
+            '@typescript-eslint/no-non-null-assertion': 'off',
+        },
+    },
+];
+
