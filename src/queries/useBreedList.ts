51c4f6ac3c951c1f0df1ec3806484b580bbff85a
mgrzmil <mgrzmil4@gmail.com>
2026-01-17 21:06:07 +0100

    add react-query, rewrite hooks to rq

---
+++ src/queries/useBreedList.ts
@@ -0,0 +1,22 @@
+import { useQuery } from '@tanstack/react-query';
+import { httpClient } from '../common';
+import { DogListDTO, DogEntries } from '../types';
+import { dogListMapper } from '../hooks/useDogList/dogListMapper';
+import { queryKeys } from './queryKeys';
+
+const fetchBreedList = async (): Promise<DogEntries> => {
+    const response = await httpClient.get<DogListDTO>('/breeds/list/all');
+
+    if (response.data.code) {
+        throw new Error(`${response.data.code} ${response.data.status}`);
+    }
+
+    return dogListMapper(response.data);
+};
+
+export const useBreedList = () => {
+    return useQuery({
+        queryKey: queryKeys.breeds.list(),
+        queryFn: fetchBreedList,
+    });
+};
